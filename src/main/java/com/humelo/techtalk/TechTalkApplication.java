c9d5a4ad4b01e2bb0eb8e4c8760f5cfb814dcf26
kiljaewon <kiljw316@gmail.com>
2023-05-12 00:50:34 +0900

    Initial commit

---
+++ src/main/java/com/humelo/techtalk/TechTalkApplication.java
@@ -0,0 +1,13 @@
+package com.humelo.techtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/humelo/techtalk/TechTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humelo.techtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
